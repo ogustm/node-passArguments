9aeee42043cb74f51892fd05a4be9f4d8459e123
ogustm <osward.gust.martinez@gmail.com>
2019-09-18 14:37:13 +0200

    programm stars and message

---
+++ index.js
@@ -0,0 +1,6 @@
+const { printingStars } = require('./stars');
+
+const stars = process.argv[2];
+const text = process.argv[3];
+printingStars(stars, text);
+
---
+++ stars.js
@@ -0,0 +1,8 @@
+
+module.exports.printingStars = (stars = 10,text = 'hi')=> {   //10 stars by default   
+    
+        console.log('*'.repeat(stars));
+        console.log(text);
+        console.log('*'.repeat(stars));    
+    
+}
